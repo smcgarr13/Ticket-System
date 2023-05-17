d74a362bd9b7493c16c5a73b54187e24c60d2021
Sparkles <smcgarr13@yahoo.com>
2023-05-16 21:09:27 -0500

    initial commentsTypeDefs file

---
+++ server/schemas/typeDefs/commentsTypeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  # Comment type definition
+  type Comment {
+    _id: ID
+    commentAuthor: String
+    commentText: String
+    createdAt: String
+    isSolution: Boolean
+  }
+
+  # Query type definition
+  type Query {
+    comment(commentId: ID!): Comment
+  }
+
+  # Mutation type definition
+  type Mutation {
+    updateComment(
+      commentId: ID!
+      isSolution: Boolean
+      commentText: String
+    ): Comment
+    addComment(
+      questionId: ID!
+      commentText: String!
+      commentAuthor: String!
+    ): Comment
+    removeComment(
+      questionId: ID!
+      commentId: ID!
+    ): Comment
+  }
+`;
+
+module.exports = typeDefs;
