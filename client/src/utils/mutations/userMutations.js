1cf22f1fc1b9497f952260e480e338a0bb615820
Luis Guillen <lag2993@gmail.com>
2023-05-18 22:02:34 -0400

    Created mutations for client utils, need to add remove user and ask TA

---
+++ client/src/utils/mutations/userMutations.js
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USERS = gql`
+mutation addUsers($username: String!, $email: String!, $password: String!){
+    token
+    user{
+        _id
+        username
+    }
+}
+`
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
