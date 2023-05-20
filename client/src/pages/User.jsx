fa843c39232cdf5f6814efd835206dadd6ddbb1e
Sparkles <smcgarr13@yahoo.com>
2023-05-20 15:39:18 -0500

    initial User page file

---
+++ client/src/pages/User.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+import UserForm from '../components/UserForm';
+import '../index.css';
+
+function User() { 
+  return (
+    <div>
+      {/* <Header /> */}
+      {/* <h2>Create an Account</h2> */}
+      <UserForm />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default User;
