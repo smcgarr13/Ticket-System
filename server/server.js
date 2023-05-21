781152ba6c92abf333dda17d771ebc2f03d50851
Luis Guillen <lag2993@gmail.com>
2023-05-21 16:49:58 -0400

    Wrote up the initial server stuff still need to set the bounty and tie it to that system

--- server/server.js
+++ server/server.js
@@ -15,8 +15,11 @@ const server = new ApolloServer({
 });
 
 // 
-app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+
 
 if(process.env.NODE_ENV ==='production'){
    app.use(express.static(path.join(__dirname,'../client/build')));
